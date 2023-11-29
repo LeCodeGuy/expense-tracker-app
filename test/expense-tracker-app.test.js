e86689adbdaeb5bf275d9b6846d1f3d85f55d9f7
LeCodeGuy <devanj89@gmail.com>
2023-11-29 12:51:50 +0200

    Added comments to the unit tests

--- test/expense-tracker-app.test.js
+++ test/expense-tracker-app.test.js
@@ -1,19 +1,26 @@
+// Import necessary modules
 import assert from "assert";
 import db from "../sql/database-connection.js";
 import queries from "../services/expense-tracker-services.js";
 
+// Create an instance of the queries service using the database connection
 const query= queries(db);
 
+// Test suite for the Expense Tracker App
 describe("Expense Tracker App testing", function(){
+    // Set a timeout for each test case
     this.timeout(6000);
     
+    // Before each test, reset the database to ensure a clean state
     this.beforeEach(async function (){
         await query.reset();
     });
-
-    it("should be able to add an expense", async function (){        
+    // Test case: Adding an expense
+    it("should be able to add an expense", async function (){
+        // Add an expense to the database       
         await query.addExpense("Coffee", "weekday", 30);
         
+        // Expected result after adding the expense
         const expected = [
                     {
                     id: 1,
@@ -23,21 +30,23 @@ describe("Expense Tracker App testing", function(){
                     category_id: 3
                     }
                 ]
+        // Retrieve all expenses and compare with the expected result
         let actual = await query.allExpenses();
-        
         assert.deepEqual(expected,actual);
     });
 
+    // Test case: Deleting a given expense
     it("should be able to delete a given expense", async function (){        
-        // add expenses
+        // Add expenses to the database
         await query.addExpense("Coffee", "weekday", 30);
         await query.addExpense("Rent", "monthly", 5000);
         await query.addExpense("Groceries", "weekly", 1000);
         
-        // remove expense
+        // Remove expenses from the database
         await query.deleteExpense("Coffee", "weekday");
         await query.deleteExpense("Groceries", "weekly");
 
+        // Expected result after deleting expenses
         const expected = [
             {
                 id:2,
@@ -49,17 +58,20 @@ describe("Expense Tracker App testing", function(){
             }
         ]
 
+        // Retrieve all expenses and compare with the expected result
         let actual = await query.allExpenses();
-
         assert.deepEqual(expected,actual);
     });
+    
+    // Test case: Returning expenses for a specific category
     it("should return expenses for a specific category", async function (){        
-        // add expenses
+        // Add expenses to the database
         await query.addExpense("Coffee", "weekday", 30);
         await query.addExpense("Rent", "monthly", 5000);
         await query.addExpense("Groceries", "weekly", 1000);
         await query.addExpense("Lunch", "weekday", 50);
 
+        // Expected result for expenses in the "weekday" category
         const expected = [
                 {
                     expense: 'Coffee',
@@ -70,35 +82,40 @@ describe("Expense Tracker App testing", function(){
                     total: '250'
                 }
             ]
-
+        
+        // Retrieve expenses for the "weekday" category and compare with the expected result
         let actual = await query.expensesForCategory("weekday");
-
         assert.deepEqual(expected,actual);
     });
 
+    // Test case: Returning the totals for all categories
     it("should return the totals for all the categories", async function (){        
-        // add expenses
+        // Add expenses to the database
         await query.addExpense("Coffee", "weekday", 30);
         await query.addExpense("Rent", "monthly", 5000);
         await query.addExpense("Groceries", "weekly", 1000);
         await query.addExpense("Lunch", "weekday", 50);
         
+        // Expected result for category totals
         let expected = [
             { category: 'weekday', total: '400' },
             { category: 'weekly', total: '4000' },
             { category: 'monthly', total: '5000' }
           ]
 
+        // Retrieve category totals and compare with the expected result
         let actual = await query.categoryTotals();
-
         assert.deepEqual(expected,actual);
     });
 
+    // Test case: Returning all expenses
     it("should return all expenses", async function (){        
+        // Add expenses to the database
         await query.addExpense("Coffee", "weekday", 30);
         await query.addExpense("Rent", "monthly", 5000);
         await query.addExpense("Groceries", "weekly", 1000);
         
+        // Expected result for all expenses
         const expected = [
             {
                 id: 1,
@@ -125,11 +142,12 @@ describe("Expense Tracker App testing", function(){
             }
         ]
 
+        // Retrieve all expenses and compare with the expected result
         let actual = await query.allExpenses();
-
         assert.deepEqual(expected,actual);
     });
 
+    // After all tests, close the database connection
     this.afterAll(function () {
         db.$pool.end;
     });
